f74a4adfe21069d69f30cfb5c53f9bbfe170a61d
rakshith.n <rakshith.n@promantia.com>
2021-08-20 14:50:28 +0530

    fixed permissions issue

--- ssk_emart_customizations/ssk_emart/doctype/warehouse_location/test_warehouse_location.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2021, Promantia Business Solutions Pvt Ltd and Contributors
-# See license.txt
-
-# import frappe
-import unittest
-
-class TestWarehouseLocation(unittest.TestCase):
-	pass
--- ssk_emart_customizations/ssk_emart/doctype/warehouse_location/warehouse_location.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021, Promantia Business Solutions Pvt Ltd and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Warehouse Location', {
-	// refresh: function(frm) {
-
-	// }
-});
